957a8f9521ad3b63d7727023e29dd050061cda9d
albert-nguyen <108554370+albert-nguyen@users.noreply.github.com>
2024-01-04 19:14:36 -0800

    Update test_nubbin_battery.py

--- test/test_battery/test_nubbin_battery.py
+++ test/test_battery/test_nubbin_battery.py
@@ -6,8 +6,8 @@ from battery.nubbin_battery import NubbinBattery
 
 class TestNubbinBattery(unittest.TestCase):
     def test_needs_service_true(self):
-        current_date = date.fromisoformat("2020-05-15")
-        last_service_date = date.fromisoformat("2016-01-25")
+        current_date = date.fromisoformat("2022-07-15")
+        last_service_date = date.fromisoformat("2017-04-24")
         battery = NubbinBattery(current_date, last_service_date)
         self.assertTrue(battery.needs_service())
 
